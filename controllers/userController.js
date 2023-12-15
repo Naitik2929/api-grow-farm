fd88a852ea6cf4c3c0adc850a14bc672ec42692f
vikrampatel0408 <vikrampatel2313>
2023-12-15 15:44:26 +0530

    authUser added

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,26 +2,47 @@ import asyncHandler from "express-async-handler";
 import User from "../models/User.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-    const { name, phoneNumber, password ,email} = req.body;
+    const { name, phoneNumber, password, email } = req.body;
     const userExist = await User.findOne({ phoneNumber });
     // console.log(userExist);
     if (userExist) {
-      res.status(400);
-      throw new Error("User alreasy exists");
+        res.status(400);
+        throw new Error("User alreasy exists");
     }
-    
-    const user = await User.create({ name, phoneNumber, password,email});
+
+    const user = await User.create({ name, phoneNumber, password, email });
     if (user) {
-      
-      res.status(200).json({
-        _id: user._id,
-        phoneNumber: user.phoneNumber,
-        name: user.name,
-      });
+
+        res.status(200).json({
+            _id: user._id,
+            phoneNumber: user.phoneNumber,
+            name: user.name,
+        });
     } else {
-      res.status(400);
-      throw new Error("Invalid user data");
+        res.status(400);
+        throw new Error("Invalid user data");
     }
-  });
-  
-export{registerUser};
+});
+const authUser = asyncHandler(async (req, res) => {
+    const { phoneNumber, password } = req.body;
+    const user = await User.findOne({ phoneNumber });
+
+    if (user && (await user.matchPassword(password))) {
+        generateToken(res, user._id);
+        res.json({
+            _id: user._id,
+            name: user.name,
+            phoneNumber: user.phoneNumber,
+            createdAt: user.createdAt,
+            updatedAt: user.updatedAt,
+        });
+    }
+     else {
+        res.status(401);
+        throw new Error("invalid password or email");
+    }
+});
+
+
+
+export { registerUser , authUser};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,8 @@
 import express from "express";
-import { registerUser } from "../controllers/userController.js";
+import { registerUser,authUser } from "../controllers/userController.js";
 const router = express.Router();
 
 router.post('/',registerUser);
+router.post('/auth',authUser);
+
 export default router ;
