bb8c72c8e786b81d1c544c3416073159170c8067
Naitik Patel <naitikpatel107@gmail.com>
2023-12-15 16:08:31 +0530

    test

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,7 @@ import { sendOTP, verifyOTP } from "../controllers/otpController.js";
 const router = express.Router();
 
 router.post("/", registerUser);
+// test
 router.post("/auth", authUser);
 router.get("/twilio-sms/send-otp", sendOTP);
 router.get("/twilio-sms/verify-otp", verifyOTP);
